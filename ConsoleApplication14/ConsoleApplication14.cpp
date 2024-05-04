daeabe62510891c42d388155178722e9a2b6dc8b
kerk222 <qwertyullkiuy@gmail.com>
2024-05-04 15:09:14 +0500

    добавленно русский язык

--- ConsoleApplication14/ConsoleApplication14.cpp
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -11,9 +11,10 @@ void bubbleSort(int arr[], int n)
 { if (arr[j] < arr[j+1])
 { int temp = arr[j]; arr[j] = arr[j+1]; arr[j+1] = temp; } } } }
 int main() {
+	setlocale(0, "RUS");
 	int arr[] = {64, 34, 25, 12, 22, 11, 90};
 	int n = sizeof(arr) / sizeof(arr[0]); bubbleSort(arr, n);
-	cout << "Sorted array: ";
+	cout << "сортировка аrr: ";
 	for (int i = 0; i < n; i++)
 		cout << arr[i] << " "; cout << endl; 
 	return 0;
